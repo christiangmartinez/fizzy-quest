d9fdb36c94997dd8d784f941e33a1801af6ed536
Christian Martinez <cgmartinez11142@gmail.com>
2017-06-03 17:58:57 -0700

    add welcome message

--- app/src/main/java/io/xtian/fizzyquest/ui/MainActivity.java
+++ app/src/main/java/io/xtian/fizzyquest/ui/MainActivity.java
@@ -1,15 +1,23 @@
 package io.xtian.fizzyquest.ui;
 
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
 import io.xtian.fizzyquest.R;
 
 public class MainActivity extends AppCompatActivity {
+    @Bind(R.id.titleView) TextView mTitleView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        Typeface pixelFont = Typeface.createFromAsset(getAssets(), "fonts/slkscrb.ttf");
+        mTitleView.setTypeface(pixelFont);
     }
 }
