80abb24cbd2abe57afa28f5ae2059bfb1861a6d7
Christian Martinez <cgmartinez11142@gmail.com>
2017-06-03 18:33:55 -0700

    save user input

---
+++ app/src/main/java/io/xtian/fizzyquest/services/BrewService.java
@@ -0,0 +1,8 @@
+package io.xtian.fizzyquest.services;
+
+/**
+ * Created by Guest on 6/3/17.
+ */
+
+public class BrewService {
+}
--- app/src/main/java/io/xtian/fizzyquest/ui/SearchActivity.java
+++ app/src/main/java/io/xtian/fizzyquest/ui/SearchActivity.java
@@ -2,14 +2,37 @@ package io.xtian.fizzyquest.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
 import io.xtian.fizzyquest.R;
 
-public class SearchActivity extends AppCompatActivity {
+public class SearchActivity extends AppCompatActivity implements View.OnClickListener{
+    @Bind(R.id.userParams) EditText mUserParams;
+    @Bind(R.id.searchButton)Button mSearchButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+        ButterKnife.bind(this);
+        mSearchButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == mSearchButton) {
+            String searchTerm = mUserParams.getText().toString();
+            if (searchTerm.equals("")) {
+                Toast.makeText(SearchActivity.this, "Please fill out search field", Toast.LENGTH_LONG).show();
+            } else {
+                Log.d("searchTerm", searchTerm);
+            }
+        }
     }
 }
