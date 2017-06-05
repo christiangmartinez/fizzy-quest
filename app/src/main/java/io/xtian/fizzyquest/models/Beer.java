3a2b8ff065f7cc4159d81b60aebffa8608b97949
Christian Martinez <cgmartinez11142@gmail.com>
2017-06-04 18:35:17 -0700

    add Beer model

---
+++ app/src/main/java/io/xtian/fizzyquest/models/Beer.java
@@ -0,0 +1,37 @@
+package io.xtian.fizzyquest.models;
+
+public class Beer {
+    private String mBeerId;
+    private String mName;
+    private String mDescription;
+    private String mAbv;
+    private String mIbu;
+
+    public Beer(String beerId, String name, String description, String abv, String ibu) {
+        this.mBeerId = beerId;
+        this.mName = name;
+        this.mDescription = description;
+        this.mAbv = abv;
+        this.mIbu = ibu;
+    }
+
+    public String getBeerId() {
+        return mBeerId;
+    }
+
+    public String getName() {
+        return mName;
+    }
+
+    public String getDescription() {
+        return mDescription;
+    }
+
+    public String getAbv() {
+        return mAbv;
+    }
+
+    public String getIbu() {
+        return mIbu;
+    }
+}
