9c680878e15e9d26726bcf06d68b5a0992847b25
Christian Martinez <cgmartinez11142@gmail.com>
2017-06-04 21:48:48 -0700

    add adapter package

---
+++ app/src/main/java/io/xtian/fizzyquest/adapters/BeerAdapter.java
@@ -0,0 +1,8 @@
+package io.xtian.fizzyquest.adapters;
+
+/**
+ * Created by michellewisner on 6/4/17.
+ */
+
+public class BeerAdapter {
+}
--- app/src/main/java/io/xtian/fizzyquest/services/FizzyService.java
+++ app/src/main/java/io/xtian/fizzyquest/services/FizzyService.java
@@ -2,12 +2,21 @@ package io.xtian.fizzyquest.services;
 
 import android.util.Log;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
 import io.xtian.fizzyquest.Constants;
+import io.xtian.fizzyquest.models.Beer;
 import okhttp3.Call;
 import okhttp3.Callback;
 import okhttp3.HttpUrl;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
+import okhttp3.Response;
 
 public class FizzyService {
     public static void findBeers(String styleId, Callback callback) {
@@ -20,5 +29,32 @@ public class FizzyService {
         Request request = new Request.Builder().url(url).build();
         Call call = client.newCall(request);
         call.enqueue(callback);
+    }
+
+    public ArrayList<Beer> processResults(Response response) {
+        ArrayList<Beer> beers = new ArrayList<>();
+        try {
+            String jsonData = response.body().string();
+            if (response.isSuccessful()) {
+                JSONObject fizzyJSON = new JSONObject(jsonData);
+                JSONArray dataJSON = fizzyJSON.getJSONArray("data");
+                for (int i = 0; i < dataJSON.length(); i++) {
+                    JSONObject beerJSON = dataJSON.getJSONObject(i);
+                    String beerId = beerJSON.getString("id");
+                    String name = beerJSON.getString("nameDisplay");
+                    String description = beerJSON.getString("description");
+                    String abv = beerJSON.getString("abv");
+                    String ibu = beerJSON.getString("ibu");
+
+                    Beer beer = new Beer(beerId, name, description, abv, ibu);
+                    beers.add(beer);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
+            e.printStackTrace();;
+        }
+        return  beers;
     }
 }
