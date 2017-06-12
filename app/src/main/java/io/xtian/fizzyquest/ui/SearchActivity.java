064f6451231a645e6cd31a3260198d82646cc23d
Christian Martinez <cgmartinez11142@gmail.com>
2017-06-12 00:16:25 -0700

    add logout functionality

--- app/src/main/java/io/xtian/fizzyquest/ui/SearchActivity.java
+++ app/src/main/java/io/xtian/fizzyquest/ui/SearchActivity.java
@@ -4,11 +4,13 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
